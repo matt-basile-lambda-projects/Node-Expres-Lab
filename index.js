4e1e0c18fbae6617b69e40db5fb5da02fa5b6e98
Matt Basile <matthewbasile2@gmail.com>
2019-02-14 15:36:59 -0500

    added dyanmic ports

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+require('dotenv').config();
 const express = require('express')
 const postRouter = require('./post-Router');
+const cors = require('cors')
 const server = express();
 
 server.use(express.json());
+server.use(cors())
 server.use('/api/posts', postRouter)
 
+const port = process.env.PORT || 5000;
 // Home Route Running
 server.get('/', (req, res) => {
     res.send('Hello from EXPRESS LAB')
 });
 
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+  });
   
-
-server.listen(8000, () => console.log('API running on port 8000'));
